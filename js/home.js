1a8d960a2fae3e4e602402c6029ad8982ed8acaf
madhuvayya <madhu.vayya@gmail.com>
2020-12-18 15:52:21 +0530

    Added code to get details from JSON object

--- js/home.js
+++ js/home.js
@@ -3,27 +3,44 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 const createInnerHtml = () => {
-    const innerHtml = `
+    const headerHtml = "<tr><th>Fullname</th><th>Address</th><th>City</th><th>State</th>"
+    + "<th>Zip code</th><th>Phone Number</th><th></th></tr>";
+
+    let contactData = createContactsJson()[0]; 
+    const innerHtml = `${headerHtml}
         <tr>
-            <th>Fullname</th>
-            <th>Address</th>
-            <th>City</th>
-            <th>State</th>
-            <th>Zip code</th>
-            <th>Phone Number</th>
-            <th></th>
-        </tr>
-        <tr>
-            <td>Madhu Vayya</td>
-            <td>Subhashchandrabose nagar,Kukatpally</td>
-            <td>Hyderabad</td>
-            <td>Telangana</td>
-            <td>500072</td>
-            <td>7777777777</td>
+            <td>${contactData._fullName}</td>
+            <td>${contactData._address}</td>
+            <td>${contactData._city}</td>
+            <td>${contactData._state}</td>
+            <td>${contactData._zipcode}</td>
+            <td>${contactData._phoneNumber}</td>
             <td>
                 <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
                 <img id="1" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">                    
             </td>
         </tr>`;
     document.querySelector('#display').innerHTML = innerHtml;        
-}          
+}
+
+const createContactsJson = () => {
+    let contactsListLocal = [
+        {
+            _fullName: 'Madhu Vayya',
+            _phoneNumber: 7777777777,
+            _address: 'Subhashchandrabose nagar,Kukatpally',
+            _city: 'Hyderabad',
+            _state: 'Telangana',
+            _zipcode: '500072'
+        },
+        {
+            _fullName: 'Rajesh',
+            _phoneNumber: 7777777778,
+            _address: 'Nehru nagar',
+            _city: 'Warangal',
+            _state: 'Telangana',
+            _zipcode: '5045612'  
+        }        
+    ];
+    return contactsListLocal;
+}
