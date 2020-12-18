8521798d3ac4acb74fff1174372bb7b71b380c51
madhuvayya <madhu.vayya@gmail.com>
2020-12-18 16:36:03 +0530

    Added code to remove contact from address book entries

--- js/home.js
+++ js/home.js
@@ -27,10 +27,22 @@ const createInnerHtml = () => {
             <td>${contactData._zipcode}</td>
             <td>${contactData._phoneNumber}</td>
             <td>
-                <img name="${contactData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                <img name="${contactData._id}" onclick="remove(${contactData._id})" alt="delete" src="../assets/icons/delete-black-18dp.svg">
                 <img name="${contactData._id}" alt="edit" src="../assets/icons/create-black-18dp.svg">                    
             </td>
         </tr>`;
     }    
     document.querySelector('#display').innerHTML = innerHtml;        
+}
+
+const remove = (id) => {
+    let contactData = contactsList.find(contact => contact._id == id);
+    if (!contactData) return;
+    const index = contactsList
+                    .map(contact => contact._id)
+                    .indexOf(contactData._id);
+    contactsList.splice(index, 1);
+    localStorage.setItem("ContactsList",JSON.stringify(contactsList));
+    document.querySelector(".contacts-count").textContent = contactsList.length;
+    createInnerHtml();                
 }
